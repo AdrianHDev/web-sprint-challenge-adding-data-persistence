1a7d6e69a62e6d1d585543333dd6ccc2af2cd171
AdrianHDev <adrianhensondev@gmail.com>
2021-07-16 11:50:11 -0400

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,19 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+const getAllProjects = () => {
+    return db('projects');
+} 
+
+const getProjectById = id => {
+    return db('projects').where({project_id: id})
+}
+
+const addNewProject = project => {
+    return db('projects').create(project).then(id => getProjectById(id))
+}
+
+module.exports = {
+    getAllProjects,
+    addNewProject
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,19 @@
-// build your `/api/projects` router here
+const express = require("express");
+const router = express.Router();
+const { getAllProjects, addNewProject } = require("./model");
+router.get('/', async (req, res) => {
+    const projects = await getAllProjects();
+    res(projects)
+})
+
+router.post('/', async (req, res) => {
+    try {   
+        const newProject = await addNewProject(res.body)
+        res.status(201).json(newProject)
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'internal server error'})
+    }
+})
+
+module.exports = router;
